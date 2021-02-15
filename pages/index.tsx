d8b07351029d9a076e6def376a26e9d63d0e034e
Sean Harte <scsharte@gmail.com>
2021-02-15 17:22:23 -0500

    fixed linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 
-const Home = () =>  {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
