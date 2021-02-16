d7220253be1b6f33a210c9fed492c9f3c7b69e6f
Sean Harte <scsharte@gmail.com>
2021-02-15 20:07:03 -0500

    added chakra and themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import '../styles/global.scss'
-
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../next-seo.config'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../theme'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ChakraProvider resetCSS theme={theme}>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
 
