0d2919e355af6edba57492624b3adfe2362a5e59
Sean Harte <scsharte@gmail.com>
2021-02-16 22:07:54 -0500

    using default meme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,36 @@
 import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+// import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { mono: `'Menlo', monospace` }
+// const fonts = { mono: `'Menlo', monospace` }
 
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+// const breakpoints = createBreakpoints({
+//   sm: '40em',
+//   md: '52em',
+//   lg: '64em',
+//   xl: '80em',
+//   "2xl": '100%'
+// })
+
+// const Button: ThemeComponents = {
+//   baseStyle: {
+//     fontWeight: "bold",
+//     textTransform: "uppercase",
+//     _hover: {
+//       textDecoration: "none",
+//     }
+//   }
+// }
 
 const theme = extendTheme({
-  breakpoints,
-  colors: {
-    black: '#16161D',
+  components: {
+    Link: {
+      baseStyle: {
+        _hover: {
+          textDecoration: "none",
+        }
+      }
+    }
   },
-  fonts,
 })
 
 export default theme
