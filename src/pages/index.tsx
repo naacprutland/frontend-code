b1c69d9e230b61cade75cfb4b258c30d1a1db1c0
Sean Harte <scsharte@gmail.com>
2021-02-20 23:14:46 -0500

    cleaned up index and moved data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import {
   ListItem,
 } from '@chakra-ui/react'
 import { Icon } from "@chakra-ui/react"
-import { AiFillCheckCircle, AiOutlineLink } from "react-icons/ai";
+import { AiFillCheckCircle, AiOutlineLink } from "react-icons/ai"
 
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
@@ -14,41 +14,15 @@ import { Main } from '../components/Main'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
-import { NextSeo } from 'next-seo';
+import { NextSeo } from 'next-seo'
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
 import { GetStaticProps } from 'next'
 
-const seo = {
-  "title": "Home Page",
-  "description": "description of home page",
-  "canonical": "https://www.example.com",
-  "openGraph": {
-    "url": "https://www.example.com",
-    "title": "Home Page",
-    "description": "description of home page",
-    "site_name": "Page Name",
-    "images": [
-      {
-        "url": "https://www.example.ie/og-image-01.jpg",
-        "width": 800,
-        "height": 600,
-        "alt": "Og Image Alt"
-      },
-      {
-        "url": "https://www.example.ie/og-image-02.jpg",
-        "width": 900,
-        "height": 800,
-        "alt": "Og Image Alt Second"
-      },
-      { "url": "https://www.example.ie/og-image-03.jpg" },
-      { "url": "https://www.example.ie/og-image-04.jpg" }
-    ]
-  }
-};
 
-const Index = () => (
+const Index = ({data ={}}) => (
   <Container height="100vh">
-    <NextSeo {...seo} />
+    {console.log(data)}
+    <NextSeo {...data.seo} />
     <Hero />
     <Main>
       <Text>
@@ -86,53 +60,35 @@ const Index = () => (
   </Container>
 )
 
-export default Index
-
-// import Head from 'next/head'
-// /**
-//  * Import helpers and GetStaticProps type
-//  */
-// import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
-// import { GetStaticProps } from 'next'
-
-// export default function Home({ file }) {
-//   const data = file.data
-
-//   return (
-//     <div className="container">
-//       <Head>
-//         <title>Create Next App</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main>
-//         <h1 className="title">
-//           {/**
-//            * Render the title from `home.json`
-//            */}
-//            Welcome to <a href="https://nextjs.org">Next.js!</a>
-//           {data.title}
-//         </h1>
-//       </main>
-
-//     </div>
-//   )
-// }
-
 // /**
 //  * Fetch data with getStaticProps based on 'preview' mode
 //  */
 export const getStaticProps: GetStaticProps = async function({
-  preview,
+  preview = false,
   previewData,
  }) {
   if (preview) {
-    console.log('preview', previewData);
-    return getGithubPreviewProps({
+    console.log('preview mode', previewData);
+    const prevProp = {
       ...previewData,
-      fileRelativePath: 'src/data/home.json',
+      fileRelativePath: '/src/data/home.md',
       parse: parseJson,
-    })
+    }
+    console.log(prevProp)
+
+    // const gitHubProps = await getGithubPreviewProps(prevProp)
+
+    // console.log(gitHubProps);
+
+    // const file = gitHubProps;
+
+    return {
+      props: {
+        preview: true,
+        fileRelativePath: 'src/data/home.json',
+        data: (await import('../data/home.json')).default
+      }
+    }
   }
   console.log('ok', previewData)
   return {
@@ -140,10 +96,10 @@ export const getStaticProps: GetStaticProps = async function({
       sourceProvider: null,
       error: null,
       preview: false,
-      file: {
-        fileRelativePath: '/data/home.json',
-        data: (await import('../data/home.json')).default,
-      },
+      fileRelativePath: '/src/data/home.json',
+      data: (await import('../data/home.json')).default,
     },
   }
  }
+
+export default Index
