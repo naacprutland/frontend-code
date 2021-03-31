43eea7bb336b5d089606a3f4bad33932c6da7206
Sean Harte <scsharte@gmail.com>
2021-03-30 23:18:45 -0400

    added story for cta and text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { Heading, Box, AspectRatio, Grid, GridItem,
-  useBreakpointValue, Container, Button, HStack  } from '@chakra-ui/react'
+  useBreakpointValue, Container, Button, Wrap, WrapItem  } from '@chakra-ui/react'
 import Image from 'next/image'
 
 import Link from 'next/link'
@@ -26,7 +26,7 @@ export interface CTABtn {
   external: boolean;
 }
 
-enum BtnAlign {
+enum AlignItems {
   start = 'flex-start',
   center = 'center',
   end = 'flex-end'
@@ -49,7 +49,7 @@ const posPropMd = {
     span: 8,
   },
   right: {
-    col: 12,
+    col: 4,
     span: 8
   },
   center: {
@@ -64,7 +64,7 @@ const posPropLG = {
     span: 6,
   },
   right: {
-    col: 12,
+    col: 6,
     span: 6
   },
   center: {
@@ -83,7 +83,7 @@ const Hero = ({
   textPos = "start", cta,
   backgroundImage, horPos = 'center', verPos = 'middle', pagePos = 0 }: HeroProps) => {
   const variant = useBreakpointValue({ base: posProMobile, md: posPropMd[horPos], lg: posPropLG[horPos] })
-  const ratio = useBreakpointValue({ base: 3 / 4, sm: 7 / 4})
+  const ratio = useBreakpointValue({ base: 3 / 4, sm: 7 / 4 })
   const decOverlay = imgOverlayPer / 100;
 
   return (
@@ -97,7 +97,7 @@ const Hero = ({
                 layout="fill"
                   objectFit="cover" objectPosition="center" {...backgroundImage} />
         </Box>
-        <Container h="100%"  maxW="100%">
+        <Container h="100%" paddingBottom="1.5rem" paddingTop="1.5rem" maxW="100%">
           <Grid
             h="100%"
             templateRows="repeat(3, 1fr)"
@@ -105,6 +105,9 @@ const Hero = ({
             {(title || detail) && <GridItem rowStart={VerPos[verPos]}
                 alignSelf="center"
                 textAlign={textPos}
+                d="flex"
+                flexDirection="column"
+                alignItems={AlignItems[textPos]}
                 color={theme === 'dark' ? 'white' : 'black'}
                 colStart={variant?.col}
                 colSpan={variant?.span}>
@@ -112,12 +115,13 @@ const Hero = ({
                   {title}
                 </Heading>
                 }
-                {detail && <Box maxW="300px">{detail}</Box>}
-                {(cta?.length > 0) && <HStack justifyContent={BtnAlign[textPos]} spacing={2} py={2}>
+                {detail && <Box maxW="lg">{detail}</Box>}
+                {(cta?.length > 0) && <Wrap justify={AlignItems[textPos]} spacing={2} py={2}>
                     {cta && cta.map(({label, link, external}, i) => {
                       if ( external) {
-                        return <Button as="a"
-                                key={label + i}
+                        return <WrapItem key={label + i}>
+                            <Button as="a"
+                                
                                 href={link}
                                 target="_blank"
                                 rel="noopener noreferrer"
@@ -125,15 +129,16 @@ const Hero = ({
                                 colorScheme="purple" size="md">
                               {label}
                               </Button>
+                            </WrapItem>
                       }
-                      return (<Link key={label + i} href={link}>
+                      return (<WrapItem key={label + i}><Link href={link}>
                             <Button as="a"
                               href=""
                               cursor="pointer"
                               colorScheme="purple" size="md">{label}</Button>     
-                        </Link>)
-                      })}
-                  </HStack>}
+                        </Link></WrapItem>)
+                      }).slice(0, 4)}
+                  </Wrap>}
               </GridItem>}
           </Grid>
         </Container>
--- src/stories/Hero.stories.tsx
+++ src/stories/Hero.stories.tsx
@@ -15,8 +15,8 @@ export default {
 
 const Template: Story<HeroProps> = (args) => <Hero {...args} />;
 
-export const ctaButtons = Template.bind({});
-ctaButtons.args = {
+export const maxOfFourCta = Template.bind({});
+maxOfFourCta.args = {
   title: "Harte Hero",
   backgroundImage: {
     src: testImg1,
@@ -38,9 +38,32 @@ ctaButtons.args = {
       label: "Button",
       link: "https://www.google.com/",
       external: true
-    }
+    },
+    {
+      label: "Button",
+      link: "/about",
+      external: false
+    },
+    {
+      label: "Button",
+      link: "/about",
+      external: false
+    },
   ],
 };
 
-// export const LoggedOut = Template.bind({});
-// LoggedOut.args = {};
+export const subText = Template.bind({});
+subText.args = {
+  title: "Harte Hero",
+  detail:"Panda Ipsum the genus Ailurus and eat bamboo in the forest as three days. That is one of pandas, the cool tiny panda red panda bear giant panda is cute panda. Panda Ipsum the only ovulate once each year. During that ovulation period, they are only living species of the reasons.",
+  backgroundImage: {
+    src: testImg1,
+    alt: "the city"
+  },
+  theme: "dark",
+  imgOverlayPer: 50,
+  horPos: "center",
+  verPos: "bottom",
+  textPos: "start",
+  pagePos: 0,
+};
