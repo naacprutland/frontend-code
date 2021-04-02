e1492f03a8e5f9767bbbac368c80b06ae11b71fb
Sean Harte <scsharte@gmail.com>
2021-04-02 15:13:29 -0400

    pulled themes into the styled components

--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -1,6 +1,7 @@
-import { Container, Box, CSSObject } from "@chakra-ui/react"
+import { Container, Box, useTheme } from "@chakra-ui/react"
 import CtaList, { CTABtn, BtnColor, BtnVariant } from './CtaList'
 import { AlignItemsOptions } from '../interface/enums'
+import styled from 'styled-components'
 
 export interface TextBlockProps {
   richText: string;
@@ -10,38 +11,71 @@ export interface TextBlockProps {
   variant: BtnVariant;
 }
 
-export const contentStyling: CSSObject = {
-  h1: {
-    fontSize: "5xl",
-    fontWeight: "bold"
-  },
-  h2: {
-    fontSize: "4xl",
-    fontWeight: "bold"
-  },
-  h3: {
-    fontSize: "3xl",
-    fontWeight: "bold"
-  },
-  p: {
-    minHeight: 6
-  },
-  "ol, ul": {
-    marginLeft: 7
-  },
-  a: {
-    color: "blue.500"
+export const ContentContainer = styled(Box)`
+  h1, h2, h3 {
+    font-weight: ${({ fontWeights }) => fontWeights.bold};
   }
-}
+
+  h1 {
+    font-size: ${({ fontSizes }) => fontSizes['5xl']};
+  }
+
+  h2 {
+    font-size: ${({ fontSizes }) => fontSizes['4xl']};
+  }
+
+  h3 {
+    font-size: ${({ fontSizes }) => fontSizes['3xl']};
+  }
+
+  p {
+    min-height: ${({ space }) => space['6']};
+  }
+
+  ol,
+  ul {
+    margin-left: ${({ space }) => space['7']};
+  }
+
+  a {
+    color: ${({ colors }) => colors.blue[500]};
+    position: relative;
+    transition: all 0.25s;
+
+    &::after {
+      border-bottom: ${({ space }) => space.px} solid ${({ colors }) => colors.blue[700]};
+      bottom: 0;
+      content: '';
+      height: 0;
+      left: 0;
+      position: absolute;
+      transition: all 0.25s;
+      width: 0;
+    }
+
+    &:hover {
+      color: ${({ colors }) => colors.blue[700]};
+
+      &::after {
+        content: '';
+        width: 100%;
+      }
+    }
+  }
+`
 
 function createMarkup(richText: string) {
   return {__html: richText};
 }
 
-const TextBlock = ({ richText, ...ctaListProps }: TextBlockProps)=> (
-<Container maxW="container.md" centerContent>
-    <Box width="100%" sx={contentStyling} dangerouslySetInnerHTML={createMarkup(richText)}></Box>
-    <CtaList size="md" {...ctaListProps} />
-</Container>)
+const TextBlock = ({ richText, ...ctaListProps }: TextBlockProps)=> {
+  const theme = useTheme()
+  return (
+    <Container maxW="container.md" centerContent>
+        <ContentContainer {...theme} dangerouslySetInnerHTML={createMarkup(richText)} />
+        <CtaList size="md" {...ctaListProps} />
+    </Container>
+  )
+}
 
 export default TextBlock
