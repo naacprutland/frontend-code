836f1baa3c76844f0a7d7857b0657e3fb6987718
Sean Harte <scsharte@gmail.com>
2021-04-02 15:39:26 -0400

    Added text block to the dynamic component

--- src/components/DynamicComponent.tsx
+++ src/components/DynamicComponent.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import { ComponentType } from 'react'
 import dynamic from 'next/dynamic'
-import type { Block } from '../interface/componentBlock'
+import type { Block, HeroBlock, TextBlock } from '../interface/componentBlock'
 import { HeroProps } from './HeroBlock'
+import { TextBlockProps } from './TextBlock'
 
-const DynamicHero: React.ComponentType<HeroProps> = dynamic(() => import('./HeroBlock'))
+const DynamicHero: ComponentType<HeroProps> = dynamic(() => import('./HeroBlock'))
+const DynamicTextBlock: ComponentType<TextBlockProps> = dynamic(() => import('./TextBlock'))
 
 function DynamicComponent({ _template, ...props}: Block) {
   switch (_template) {
     case 'hero-block':
-      return <DynamicHero {...props}/>
+      return <DynamicHero {...props as HeroBlock}/>
+    case 'text-block':
+      return <DynamicTextBlock {...props as TextBlock}/>
     default :
       return null
   }
--- src/interface/componentBlock.ts
+++ src/interface/componentBlock.ts
@@ -1,4 +1,5 @@
 import { HeroProps } from '../components/HeroBlock'
+import { TextBlockProps } from '../components/TextBlock'
 
 interface ComponentBlock {
   "_template": string;
@@ -8,6 +9,10 @@ export interface HeroBlock extends ComponentBlock, HeroProps {
   "_template": 'hero-block'
 }
 
-export type Block = HeroBlock
+export interface TextBlock extends ComponentBlock, TextBlockProps {
+  "_template": 'text-block'
+}
+
+export type Block = HeroBlock | TextBlock
 
 export type PageBlocks = Block[]
