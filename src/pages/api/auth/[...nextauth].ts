677676b99e86703b1cf2106930aabaf77e363641
Sean Harte <scsharte@gmail.com>
2021-04-30 20:13:42 -0400

    set up auth api

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+interface Credentials {
+  csrfToken: string;
+  username: string;
+  password: string;
+}
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+}
+
+const getUser = (credentials: Credentials): User | null => {
+
+  const approvedUser = {
+    id: 1,
+    username: 'hartes',
+    password: 'test'
+  }
+
+  if (credentials.username !== approvedUser.username) return null
+
+  if (credentials.password !== approvedUser.password) return null
+
+  return approvedUser
+}
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Email" },
+        password: {  label: "Password", type: "password" }
+      },
+     async authorize(credentials: Credentials) {
+        console.log(credentials)
+        const user = getUser(credentials)
+        if (user) {
+          // Any user object returned here will be saved in the JSON Web Token
+          return user
+        } else {
+          return null
+        }
+      }
+    })
+  ]
+})
