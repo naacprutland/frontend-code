38048fd9ac61bd36c84b9eef1f99cc898cea5034
Sean Harte <scsharte@gmail.com>
2021-06-28 16:34:25 -0400

    removed config env

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    REPO_FULL_NAME: process.env.REPO_FULL_NAME,
-    BASE_BRANCH: process.env.BASE_BRANCH
-  },
+    // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    // REPO_FULL_NAME: process.env.REPO_FULL_NAME,
+    // BASE_BRANCH: process.env.BASE_BRANCH
+  }
 }
