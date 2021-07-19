0f382119dde4927cda06537a255458d5423268a9
Sean Harte <scsharte@gmail.com>
2021-07-19 02:23:35 -0400

    intersection observer hook

---
+++ src/hooks/intersectionObserver.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect, RefObject} from 'react'
+
+/**
+ *  indicates when an element is in view
+ * @param reference = Element reference
+ * @returns boolean
+ */
+const useIntersectionObserver = (reference: RefObject<T>): boolean => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleIntersect = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        setIsVisible(true);
+        observer.unobserve(entries[0].target);
+        observer.disconnect();
+      }
+    }
+
+		// Create the observer, passing in the callback
+    const observer = new IntersectionObserver(handleIntersect);
+
+		// If we have a ref value, start observing it
+    if (reference) {
+      observer.observe(reference.current);
+    }
+
+    // If unmounting, disconnect the observer
+    return () => observer.disconnect();
+  }, [reference]);
+
+  return isVisible;
+};
+
+export default useIntersectionObserver
