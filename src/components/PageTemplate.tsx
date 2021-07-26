404d4db1a514cbf3b2b64329d16c60f00a7aee10
Sean Harte <scsharte@gmail.com>
2021-07-26 17:04:19 -0400

    cleaned up page template

--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -2,17 +2,15 @@ import { NextSeo } from 'next-seo'
 import { PageTemplateProps } from '../interface/page'
 import DynamicComponent from './DynamicComponent'
 
-const PageTemplate = ({ pageSEO,  pageStructure }: PageTemplateProps) => {
-  console.log(pageStructure)
-  return (
-    <>
-      <NextSeo {...pageSEO} />
-      {pageStructure?.map((cProps, i) => {
-        return <DynamicComponent 
-          key={`pageComp${i}`}
-          {...cProps} position={i}/>
-      })}
-    </>
-)}
+const PageTemplate = ({ pageSEO,  pageStructure }: PageTemplateProps) => (
+  <>
+    <NextSeo {...pageSEO} />
+    {pageStructure?.map((cProps, i) => (
+      <DynamicComponent 
+        key={`pageComp${i}`}
+        {...cProps} position={i}/>
+    ))}
+  </>
+)
 
 export default PageTemplate
