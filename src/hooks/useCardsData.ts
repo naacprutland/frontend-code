a40c42f52673152ffe9d1c4fb1ef16c8e0da1d87
Sean Harte <scsharte@gmail.com>
2021-08-08 02:58:20 -0400

    made custom hook

---
+++ src/hooks/useCardsData.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { CardProps } from '../components/Card'
+import { ResultItem } from '../components/ContentLandingPage'
+
+type Tuple = [CardProps[], (results?: ResultItem[]) => void];
+
+const useCardData = (results: ResultItem[] = []): Tuple => {
+  const [data, setData] = useState<CardProps[]>([])
+
+  const setCardsData = (results: ResultItem[] = []) => {
+    const adjustedData: CardProps[] = results.map(val => {
+      return {
+        title: val.title,
+        copy: val.description,
+        image: val.image,
+        link: {
+          label: 'View Article',
+          path: val?.page?.path,
+          isExternal: false
+        }
+      }
+    })
+
+    setData(adjustedData)
+  }
+
+  useEffect(() => {
+    setCardsData(results)
+  }, [results])
+
+  return [data, setCardsData]
+}
+
+export default useCardData
