24c884afad7617338ecf27a521050004317fe21a
Sean Harte <scsharte@gmail.com>
2021-08-30 00:03:31 -0400

    interface page response from api

---
+++ src/interface/pageResponse.ts
@@ -0,0 +1,28 @@
+import { ResponseBlocks } from './componentBlock';
+import { SEO } from './seo'
+
+export interface PageResponseProps {
+  homePage: boolean;
+  _id: string;
+  id: string;
+  published_at: Date;
+  createdAt: Date;
+  updatedAt: Date
+  path: string;
+  category: null | unknown;
+  static_path: { slug: string[] };
+  keywords:{
+    pages: [];
+    _id: string;
+    wordOrPhrase: string;
+    slug: string;
+    createdAt: Date;
+    updatedAt: Date;
+    __v: number;
+    id: string;
+  }[];
+  slug: string;
+  label: string;
+  pageSEO: SEO;
+  pageStructure?: ResponseBlocks[] | null;
+}
