9375b6ac4c9c2f15ed0094be06e0d8ae1db6c018
Sean Harte <scsharte@gmail.com>
2021-08-30 00:05:28 -0400

    estabilshed page structure builder

--- src/lib/pageStructureBuilder.ts
+++ src/lib/pageStructureBuilder.ts
@@ -1,25 +1,41 @@
+import { ResultItem } from "../components/SearchSortBlock";
+import { Block, ResponseBlocks, SearchSortBlock, SearchSortRespBlock } from "../interface/componentBlock";
+import { PageTemplateProps } from "../interface/page";
+import { PageResponseProps } from "../interface/pageResponse";
+import { searchSortQuery } from "./strapiClient";
 
-// import { PageTemplateProps } from '../interface/page'
-// import { CLPProps, ResultItem } from '../components/SearchSortBlock'
-// import { searchSortQuery } from '../lib/strapiClient'
-// import { CardProps } from '../components/Card';
+interface BlockBuilder {
+  'search-sort-block': (data: SearchSortRespBlock) => Promise<SearchSortBlock>
+}
 
-// interface BlockBuilder {
-//   searchSortBlock: () => Promise<CLPProps>
-// }
+const blockBuilder: BlockBuilder = {
+  'search-sort-block': async ({ collection_type, template }: SearchSortRespBlock): Promise<SearchSortBlock> => {
+    const collectionType = collection_type
+    const results: ResultItem[] = await searchSortQuery(collectionType)
+    return {
+      template,
+      collectionType,
+      results
+    }
+  }
+}
 
-// const blockBuilder: BlockBuilder = {
-//   searchSortBlock: async (collectionType: string): Promise<CardProps[]> => {
-//     const results: ResultItem[] = await searchSortQuery(collectionType)
-//     return {
-//       collectionType,
-//       results
-//     }
-//   }
-// }
+export async function buildPageStructure(data: PageResponseProps): Promise<PageTemplateProps> {
+  const clone: PageResponseProps = JSON.parse(JSON.stringify(data))
+  const clonePageStructure: ResponseBlocks[] = clone.pageStructure || []
+  const pageStructure: Block[] = [];
+  for (const blockComp of clonePageStructure) {
+    if (blockComp.template in blockBuilder) {
+      const resp: Block = await blockBuilder[blockComp.template](blockComp)
+      pageStructure.push(resp)
+    } else {
+      pageStructure.push(blockComp as Block)
+    }
 
-export function buildPageStructure(data: number): void {
-  // eslint-disable-next-line no-console
-  console.log(data)
-  //
+  }
+  
+  return {
+    ...clone,
+    pageStructure
+  }
 }
