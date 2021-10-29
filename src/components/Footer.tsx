bc2ec792bedce367dcb0841868138b831751b4be
Sean Harte <scsharte@gmail.com>
2021-10-28 23:57:11 -0400

    fixed footer styling issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,7 @@ import { AiFillFacebook } from "react-icons/ai"
 
 const Footer = (props: FlexProps) => (
     <Box as="footer" bg="blue.900" color="white" >
-      <Container maxW="container.xl" d="flex" py="2rem" {...props} >
-        <Box>Harte Code</Box>
+      <Container maxW="container.xl" justifyContent="space-between" d="flex" py="2rem" {...props} >
         <Box>Harte Code</Box>
         <List d="flex" spacing={3} my={0}>
           <ListItem >
