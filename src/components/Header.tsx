7e2b087fc3e00c7bd2581c6d50a7c91943fe9394
hartes <hartes@orvis.com>
2022-03-08 11:32:51 -0500

    finished most of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react"
 import { 
   Box,
+  Button,
   Text,
+  VStack,
   HStack,
   useColorMode,
-  IconButton
+  IconButton,
+  useBreakpointValue
 } from "@chakra-ui/react"
 import DarkModeSwitch from '../components/DarkModeSwitch'
 import Link from 'next/link'
-import { MdMenu, MdSearch } from "react-icons/md";
+import { MdMenu, MdSearch } from "react-icons/md"
 import Container from './Container'
-import MobileMenu from './MobileMenu';
+import MobileMenu from './MobileMenu'
 import { CTA } from '../interface/general'
+import NextLink from "next/link"
+
 
 export interface MenuItem {
   label: string;
@@ -46,82 +51,113 @@ const Header = ({
   fixed,
   transparent
 }: HeaderProps) => {
-  const [ showMenu, setShowMenu ] = useState(false)
+  const [ isOpen, setShowMenu ] = useState(false)
   const { colorMode } = useColorMode()
-  const onTriggerMenu = () => { setShowMenu((cur) => !cur) };
+  const onOpen = () => { setShowMenu(true) }
+  const onClose = () => { setShowMenu(false) }
+  const variant = useBreakpointValue({ md: "md" })
+
+  useEffect(() => {
+    if (variant === "md") onClose();
+  }, [variant])
 
   return (
-    <Box as="header" d="flex" alignItems="center" 
-      top="0" position={fixed ? 'fixed' : 'static'}
+    <Box as="header" 
+      d="flex"
+      alignItems="center" 
+      top="0"
+      position={fixed ? "fixed" : "static"}
       w="100%"
-      bg={transparent ? 'none' : 
+      bg={transparent ? "none" : 
         fixed ? '#000000BF' : "black"}
-      h="3.25rem" >
+      h={["3.25rem", "4.75rem"]} >
       <Container
         d="flex"
+        alignItems="center"
         flexDir="row"
         justifyContent="space-between" 
-        py="3"
+        py={["3", "0"]}
         color={transparent && colorMode === 'light' ? "black" : "white"}
         h="100%">  
-          <Box d="flex" alignItems="center" w="100px" h="100%">
-            <Link href="/" passHref>
-              <Box as="a" h="100%" cursor="pointer" sx={{ img: {
-                height: '100%',
-                width: '100%'
-              } }}>
-                {logo && <img src={logo.src} alt={logo.alt} />}
-              </Box>
-            </Link>
-          </Box>
-          <Box display={{ base: 'flex', md: 'none' }} gap={4} h="100%">
-            <IconButton
-              variant='outline'
-              h={'100%'}
-              minW='2'
-              w='7'
-              borderRadius='50%'
-              fontSize={'16px'}
-              colorScheme='white'
-              aria-label='search'
-              icon={<MdSearch />}
-            />
-            <IconButton
-              onClick={onTriggerMenu}
-              variant='outline'
-              h={'100%'}
-              minW='2'
-              w='7'
-              borderRadius='50%'
-              fontSize={'16px'}
-              colorScheme='white'
-              aria-label='menu'
-              icon={<MdMenu />}
-            />
-          </Box>
-          <HStack as="nav"
-            display={{ base: 'none', md: 'flex' }}
-            spacing={4} h="100%" justifyContent="flex-end">
-            <HStack as="ul" spacing={3} sx={{"listStyleType": "none"}}>
+          <Link href="/" passHref>
+            <Box as="a" h="100%" maxH="2.875rem" cursor="pointer" sx={{ 
+              img: {
+                height: "100%"
+              }}}>
+              {logo && <img src={logo.src} alt={logo.alt} />}
+            </Box>
+          </Link>
+        
+          <VStack as="nav"
+            spacing={2.5}
+            h="100%"
+            alignItems="flex-end"
+            justifyContent="center">
+            <HStack spacing={{ base: "4", md: "6" }} display="flex" >
+              {ctas.map((cta, i) => (
+                <NextLink key={cta.label + i}
+                    href={cta.path} 
+                    passHref>
+                    <Button as="a"
+                      display={{ base: "none", sm: "flex" }}
+                      h="7"
+                      px="5"
+                      variant={cta.style}
+                      color={cta.style !== "outline" ? cta.textColor : null}
+                      colorScheme={cta.color}
+                      size="sm">
+                      {cta.label}
+                    </Button>
+                </NextLink>
+              ))}
+              <IconButton
+                variant="outline"
+                h="7"
+                minW="2"
+                w="7"
+                borderRadius="50%"
+                fontSize="md"
+                colorScheme="white"
+                aria-label="search"
+                icon={<MdSearch />}
+              />
+              <IconButton
+                onClick={onOpen}
+                display={{ base: "flex", md: "none" }}
+                variant="outline"
+                h="7"
+                minW="2"
+                w="7"
+                borderRadius="50%"
+                fontSize="md"
+                colorScheme="white"
+                aria-label="menu"
+                icon={<MdMenu />}
+              />
+            </HStack>
+            <HStack as="ul"
+              display={{ base: "none", md: "flex" }}
+              spacing={3}
+              sx={{ "listStyleType": "none" }}>
               {mega_menu.map((item, i) => (
                 <Box as="li" key={item.label + i} margin="0">
                   <Link href={item?.path || ''} passHref>
                     <Text as="a" cursor="pointer"
-                      fontWeight={600} 
+                      fontWeight="semibold" 
                       textTransform="capitalize">
                       {item.label}
                     </Text>
                   </Link>
                 </Box>
               ))}
+              {includeDarkMode && <DarkModeSwitch />}
             </HStack>
-            {includeDarkMode && <DarkModeSwitch />}
-          </HStack>
+          </VStack>
           <MobileMenu
             ctas={ctas}
             megaMenu={mega_menu}
-            isOpen={showMenu}
-            onClose={onTriggerMenu} />
+            isOpen={isOpen}
+            onClose={onClose} />
       </Container>
     </Box>
    )
