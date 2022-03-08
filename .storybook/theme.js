f7b41f8b25aca6d17e89f4f3ab3cf201f4af6b82
hartes <hartes@orvis.com>
2022-03-08 14:15:04 -0500

    storybook theme

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'NAACP Rutland Area',
+  brandUrl: 'https://www.google.com',
+  brandImage: './logoBlue.png',
+})
