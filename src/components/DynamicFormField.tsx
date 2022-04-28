60ec4390ce52f072b0cfb1076735cee19e7b7fc7
Sean Harte <hartes@orvis.com>
2022-04-27 22:11:17 -0400

    included fields into dynamic form

--- src/components/DynamicFormField.tsx
+++ src/components/DynamicFormField.tsx
@@ -4,11 +4,17 @@ import type { InputProps } from './Input'
 import type { SelectProps } from './Select'
 import type { NumberInputProps } from './NumberInput'
 import type { RadiosProps } from './Radios'
+import type { CheckBoxProps } from './CheckBox'
+import type { TextAreaProps } from './TextArea'
 
-export type DynamicFormFieldProps  = InputProps | SelectProps | NumberInputProps | RadiosProps
+export type DynamicFormFieldProps  = InputProps | SelectProps | NumberInputProps | RadiosProps | TextAreaProps | CheckBoxProps
 
 const DynamicInput: ComponentType<InputProps> = dynamic(() => import('./Input'))
 const DynamicSelect: ComponentType<SelectProps> = dynamic(() => import('./Select'))
+const DynamicNumberInput: ComponentType<NumberInputProps> = dynamic(() => import('./NumberInput'))
+const DynamicRadios: ComponentType<RadiosProps> = dynamic(() => import('./Radios'))
+const DynamicCheckBox: ComponentType<CheckBoxProps> = dynamic(() => import('./CheckBox'))
+const DynamicTextArea: ComponentType<TextAreaProps> = dynamic(() => import('./TextArea'))
 
 function DynamicFormField(props: DynamicFormFieldProps) {
 
@@ -20,6 +26,14 @@ function DynamicFormField(props: DynamicFormFieldProps) {
       return <DynamicInput {...props as InputProps} />
     case 'select':
       return <DynamicSelect {...props as SelectProps} />
+    case 'number':
+      return <DynamicNumberInput {...props as NumberInputProps} />
+    case 'radio':
+        return <DynamicRadios {...props as RadiosProps} />
+    case 'checkbox':
+        return <DynamicCheckBox  {...props as CheckBoxProps} />
+    case 'textarea':
+        return <DynamicTextArea {...props as TextAreaProps} />
     default :
       return null
   }
