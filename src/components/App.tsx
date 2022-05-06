253a48dac46efb0112deecb23ac6c76dc83be142
Sean Harte <hartes@orvis.com>
2022-05-06 10:35:57 -0400

    added react query:

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,27 @@
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import { ChakraProvider } from '@chakra-ui/react'
 import Fonts from '../theme/fonts'
 import theme from '../theme/theme'
 import Layout from './Layout'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider resetCSS theme={theme}>
-    <Fonts />
-    <DefaultSeo {...pageProps?.config?.defaultSeo} />
-    <Layout headerProps={pageProps?.config?.headerProps} 
-      footerProps={pageProps?.config?.footerProps}>
-      <Component {...pageProps} />
-    </Layout>
-  </ChakraProvider>
-)
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Fonts />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <DefaultSeo {...pageProps?.config?.defaultSeo} />
+          <Layout headerProps={pageProps?.config?.headerProps} 
+            footerProps={pageProps?.config?.footerProps}>
+            <Component {...pageProps} />
+          </Layout>
+        </Hydrate>
+      </QueryClientProvider>
+    </ChakraProvider>
+)}
 
 export default App
