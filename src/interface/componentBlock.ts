83126bc47bb4c5b43dc9c01901048ce037c75f30
Sean Harte <hartes@orvis.com>
2022-09-07 18:27:47 -0400

    included checkoutblock into the dynamic build

--- src/interface/componentBlock.ts
+++ src/interface/componentBlock.ts
@@ -3,7 +3,18 @@ import { TextBlockProps } from '../components/TextBlock'
 import { DeckBlockProps } from '../components/DeckBlock'
 import { MediaBlockProps } from '../components/MediaBlock'
 import { SearchSortProps } from '../components/SearchSortBlock'
-import { BreadcrumbsApi, FeatureBlockApi, FormBlockApi, HeroBlockApi, HeroTwoBlockApi, ItemCardBlockApi, MediaBlockApi, PageSearchBlockApi, StackBlockApi, TextBlockApi } from './apiBlocks'
+import {
+  BreadcrumbsApi,
+  FeatureBlockApi,
+  FormBlockApi,
+  HeroBlockApi,
+  HeroTwoBlockApi,
+  ItemCardBlockApi,
+  MediaBlockApi,
+  PageSearchBlockApi,
+  StackBlockApi,
+  TextBlockApi,
+} from './apiBlocks'
 import { StackBlockProps } from '../components/StackBlock'
 import { HeroTwoBlockProps } from '../components/HeroTwoBlock'
 import { FormBlockProps } from '../components/FormBlock'
@@ -12,81 +23,106 @@ import { BreadcrumbsProps } from '../components/Breadcrumbs'
 import { FeatureBlockProps } from '../components/FeatureBlock'
 import { ItemDeckBlockProps } from '../components/ItemDeckBlock'
 import { PageSearchBlockProps } from '../components/PageSearchBlock'
+import { CheckoutBlockProps } from '../components/CheckoutBlock'
 
 interface ComponentBlock {
-  "template": string;
-  position?: number;
+  template: string
+  position?: number
 }
 
 export interface HeroBlock extends ComponentBlock, HeroProps {
-  "template": 'blocks.hero-block'
+  template: 'blocks.hero-block'
 }
 
 export interface TextBlock extends ComponentBlock, TextBlockProps {
-  "template": 'blocks.text-block'
+  template: 'blocks.text-block'
 }
 
 export interface DeckBlock extends ComponentBlock, DeckBlockProps {
-  "template": 'blocks.deck-block'
+  template: 'blocks.deck-block'
 }
 
 export interface MediaBlock extends ComponentBlock, MediaBlockProps {
-  "template": 'blocks.media-block'
+  template: 'blocks.media-block'
 }
 
 export interface SearchSortBlock extends ComponentBlock, SearchSortProps {
-  "template": 'blocks.search-sort-block'
+  template: 'blocks.search-sort-block'
 }
 
 export interface SearchSortRespBlock extends ComponentBlock {
-  "template": 'blocks.search-sort-block',
-  collection_type: string;
-  parent_page: string;
+  template: 'blocks.search-sort-block'
+  collection_type: string
+  parent_page: string
 }
 
 export interface ContactFormBlock extends ComponentBlock {
-  "template": 'contact-form-block'
+  template: 'contact-form-block'
 }
 
 export interface StackBlock extends ComponentBlock, StackBlockProps {
-  "template": "blocks.stack-block"
+  template: 'blocks.stack-block'
 }
 
 export interface HeroTwoBlock extends ComponentBlock, HeroTwoBlockProps {
-  "template": "blocks.hero-two-block"
+  template: 'blocks.hero-two-block'
 }
 
 export interface FormBlock extends ComponentBlock, FormBlockProps {
-  "template": "blocks.form-block"
+  template: 'blocks.form-block'
 }
 
 export interface DividerBlock extends ComponentBlock, DividerBlockProps {
-  "template": "blocks.divider-block"
+  template: 'blocks.divider-block'
 }
 
 export interface Breadcrumbs extends ComponentBlock, BreadcrumbsProps {
-  "template": "blocks.breadcrumbs"
+  template: 'blocks.breadcrumbs'
 }
 
 export interface FeatureBlock extends ComponentBlock, FeatureBlockProps {
-  "template": "blocks.feature-block"
+  template: 'blocks.feature-block'
 }
 
 export interface ItemDeckBlock extends ComponentBlock, ItemDeckBlockProps {
-  "template": "blocks.item-deck-block"
+  template: 'blocks.item-deck-block'
 }
 
 export interface PageSearchBlock extends ComponentBlock, PageSearchBlockProps {
-  "template": "blocks.page-search-block"
+  template: 'blocks.page-search-block'
 }
 
-export type ResponseBlocks = HeroBlockApi | BreadcrumbsApi | TextBlockApi | 
-  MediaBlockApi | StackBlockApi | HeroTwoBlockApi | FormBlockApi | 
-  FeatureBlockApi | ItemCardBlockApi | PageSearchBlockApi
+export interface CheckoutBlock extends ComponentBlock, CheckoutBlockProps {
+  template: 'blocks.checkout-block'
+}
 
-export type Block = HeroBlock | TextBlock | DeckBlock | MediaBlock |
-   SearchSortBlock | ContactFormBlock | StackBlock | HeroTwoBlock | 
-   FormBlock | DividerBlock | Breadcrumbs | FeatureBlock | ItemDeckBlock |
-   PageSearchBlock
+export type ResponseBlocks =
+  | HeroBlockApi
+  | BreadcrumbsApi
+  | TextBlockApi
+  | MediaBlockApi
+  | StackBlockApi
+  | HeroTwoBlockApi
+  | FormBlockApi
+  | FeatureBlockApi
+  | ItemCardBlockApi
+  | PageSearchBlockApi
+
+export type Block =
+  | HeroBlock
+  | TextBlock
+  | DeckBlock
+  | MediaBlock
+  | SearchSortBlock
+  | ContactFormBlock
+  | StackBlock
+  | HeroTwoBlock
+  | FormBlock
+  | DividerBlock
+  | Breadcrumbs
+  | FeatureBlock
+  | ItemDeckBlock
+  | PageSearchBlock
+  | CheckoutBlock
 
 export type PageBlocks = Block[]
