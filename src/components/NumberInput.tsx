18167e57f0d76f3cb78e56a7d0b1714afe6d8ef1
Sean Harte <hartes@orvis.com>
2022-09-24 10:29:32 -0400

    updated number

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,62 +1,75 @@
-import { 
-    NumberInput as ChakraNumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    NumberIncrementStepper,
-    NumberDecrementStepper,
+import {
+    Input as ChakraInput,
+    InputGroup,
+    InputLeftElement,
     FormControl,
     FormLabel,
-    FormErrorMessage 
+    FormErrorMessage
 } from '@chakra-ui/react'
 import { FormNumber } from '../interface/form'
 
 export interface NumberInputProps extends FormNumber {
+    showMoneyIcon?: boolean;
     errors: {
         [x: string]: any;
     };
     register: (name: string, RegisterOptions?) => ({ onChange, onBlur, name, ref })
 }
 
+// interface Register {
+//     onChange: (event) => void
+//     ref: (elm) => void
+//     onBlur: () => void
+// }
+// type ElmRef = null | HTMLInputElement;
+
 const NumberInput = ({
     id,
     name,
     label,
     value,
+    showMoneyIcon,
     defaultValue,
     maxLength,
     minLength,
     isRequired,
     requiredMessage,
-    errors={},
+    errors = {},
     register
 }: NumberInputProps) => {
-    
+
     return (
-    <FormControl isInvalid={errors[name]}>
-        <FormLabel htmlFor={id}>{label}</FormLabel>
-        <ChakraNumberInput size='md' bg="white" 
-            value={value as number} 
-            name={name}
-            max={maxLength?.value} 
-            min={minLength?.value}
-            defaultValue={defaultValue}
+        <FormControl isInvalid={errors[name]} >
+            <FormLabel htmlFor={id}>{label}</FormLabel>
+            <InputGroup>
+                {showMoneyIcon && <InputLeftElement
+                    pointerEvents='none'
+                    color='gray.300'
+                    fontSize='1.2em'
+                >$</InputLeftElement>}
+                <ChakraInput id={id} name={name}
+                    size='md'
+                    bg="white"
+                    type='number'
+                    min={minLength?.value}
+                    max={maxLength?.value}
+                    value={value as string | number}
+                    defaultValue={defaultValue}
+                    {
+                    ...(register ? register(name, {
+                        required: isRequired ? requiredMessage : null,
+                        minLength,
+                        maxLength
+                    }) : {})
+                    } />
+            </InputGroup>
             {
-                ...(register ? register(name, {
-                        required: isRequired ? requiredMessage : null
-                    }): {})
-            } >
-            <NumberInputField />
-            <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-            </NumberInputStepper>
-        </ChakraNumberInput>
-        {
-            (errors[name]) && (
+                (errors[name]) && (
                     <FormErrorMessage>{errors[name].message}</FormErrorMessage>
                 )
-        }
-    </FormControl>
-)}
+            }
+        </FormControl >
+    )
+}
 
 export default NumberInput
